e2edb63bbe3a920c3f550366d49683ccf23a9513
Giordany Orellana <giordanyorellana@Giordanys-Air.att.net>
2015-01-22 14:41:46 -0800

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Giordany Orellana on 1/22/15.
+//  Copyright (c) 2015 Giordany Orellana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Giordany Orellana on 1/22/15.
+//  Copyright (c) 2015 Giordany Orellana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
