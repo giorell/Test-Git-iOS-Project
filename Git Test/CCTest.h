c280d71f37ba2f7a060e7c47ecdf3c9e7f6f4db2
Giordany Orellana <giorell@gmail.com>
2015-01-23 14:49:20 -0800

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Giordany Orellana on 1/23/15.
+//  Copyright (c) 2015 Giordany Orellana. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Giordany Orellana on 1/23/15.
+//  Copyright (c) 2015 Giordany Orellana. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
